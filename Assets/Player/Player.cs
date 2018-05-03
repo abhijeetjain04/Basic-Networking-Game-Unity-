489b77bf4a5a14a01dc86c1a7f0323b7fd38decd
Abhijeet Jain <abhijeetjain04@gmail.com>
2018-05-03 22:52:30 +0530

    Complete Game Upload

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+    private Vector3 inputValue;
+	
+	void Update ()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        inputValue.x = CrossPlatformInputManager.GetAxis("Vertical");
+        inputValue.y = 0f;
+        inputValue.z = CrossPlatformInputManager.GetAxis("Horizontal");
+        
+        transform.Translate(inputValue);
+	}
+    public override void OnStartLocalPlayer()
+    {
+        GetComponentInChildren<Camera>().enabled = true;
+    }
+}
---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager
+{
+    private NetworkManager Nmanager;
+
+    public void MyStartHots()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Start host");
+        StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Host started");
+    }
+
+    public override void OnStartClient(NetworkClient MyClient)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Client started requested");
+        InvokeRepeating("PrintDots", 0f, 1f);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Client is connected to IP: " + conn.address);
+        CancelInvoke();
+    }
+
+    void PrintDots()
+    {
+        Debug.Log(".");
+    }
+}
